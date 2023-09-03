ea01416c6817e66e4d25c0d41e9ef3fd0b7f230b
Irina Litvinova <neforsha@yandex.ru>
2023-09-03 20:25:02 +0300

    fix: refactoring

--- App.tsx
+++ App.tsx
@@ -9,26 +9,26 @@ import ServiceTerms from './components/bottomSheet/serviceTerms/ServiceTerms';
 //SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    'SF Pro Rounded': require('./assets/fonts/FontsFree-Net-SF-Pro-Rounded-Heavy.ttf'),
-    'SF Pro Regular': require('./assets/fonts/SF-Pro-Display-Regular.otf'),
-    'SF Pro Semibold': require('./assets/fonts/SF-Pro-Display-Semibold.otf'),
-  });
+	const [fontsLoaded] = useFonts({
+		'SF Pro Rounded': require('./assets/fonts/FontsFree-Net-SF-Pro-Rounded-Heavy.ttf'),
+		'SF Pro Regular': require('./assets/fonts/SF-Pro-Display-Regular.otf'),
+		'SF Pro Semibold': require('./assets/fonts/SF-Pro-Display-Semibold.otf')
+	});
 
-  /*const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
+	/*const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded) {
+			await SplashScreen.hideAsync();
+		}
+	}, [fontsLoaded]);
 
-  if (!fontsLoaded) {
-    return null;
-  }*/
+	if (!fontsLoaded) {
+		return null;
+	}*/
 
-  return (
-    <>
-      <SignUp/>
-    </>
-  );
+	return (
+		<>
+			<SignUp />
+		</>
+	);
 }
 
--- components/bottomSheet/BottomSheetInfo.tsx
+++ components/bottomSheet/BottomSheetInfo.tsx
@@ -3,10 +3,10 @@ import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 import { Text } from 'react-native';
 
 type PropsType = {
-	setIsOpen: (value: boolean)=>void
+	setIsOpen: (value: boolean) => void
 }
 
-export const BottomSheetInfo:FC<PropsType> = ({setIsOpen}) => {
+export const BottomSheetInfo: FC<PropsType> = ({ setIsOpen }) => {
 
 	const snapPoints = useMemo(() => ['50%', '90%'], []);
 	const bottomSheetRef = useRef<BottomSheet>(null);
@@ -21,7 +21,7 @@ export const BottomSheetInfo:FC<PropsType> = ({setIsOpen}) => {
 			}}
 
 		>
-			<BottomSheetView style={{flex: 1, alignItems: 'center'}}>
+			<BottomSheetView style={{ flex: 1, alignItems: 'center' }}>
 				<Text>Awesome 🎉</Text>
 			</BottomSheetView>
 		</BottomSheet>
